ab3af69edb0abdc2aecbcc03180f7cbe20f889e3
stephwinam <stephaniewinam@gmail.com>
2025-05-03 15:15:43 +0300

    1NF (First Normal Form)

---
+++ answers.sql
@@ -0,0 +1,14 @@
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(255),
+    Product VARCHAR(255)
+);
+
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
+VALUES
+    (101, 'John Doe', 'Laptop'),
+    (101, 'John Doe', 'Mouse'),
+    (102, 'Jane Smith', 'Tablet'),
+    (102, 'Jane Smith', 'Keyboard'),
+    (102, 'Jane Smith', 'Mouse'),
+    (103, 'Emily Clark', 'Phone');
